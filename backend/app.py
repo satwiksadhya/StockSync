5b63ac812bef678c0edb0e4f18e7f4ad5752aec4
satwiksadhya <satwiksadhya21@gmail.com>
2026-04-02 01:21:31 +0530

    deleted requirements

--- backend/app.py
+++ backend/app.py
@@ -216,6 +216,9 @@ def test():
 
 
 # ---------- RUN ----------
+
+import os
+
 if __name__ == "__main__":
-    print("Frontend path:", FRONTEND_PATH)
-    app.run(debug=True, port=5000)
+    port  = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
